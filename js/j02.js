8b63e92202e7861f857fdad18008a40b430b145f
n7dd29c <n7dd29c@gmail.com>
2025-06-03 09:00:00 +0900

    ibm x redhat 14일차

---
+++ js/j02.js
@@ -0,0 +1,12 @@
+let age = 22;
+console.log(age)
+
+age = 30
+console.log(age)
+
+aaa = 33
+bbb = 44
+console.log(aaa + bbb)
+
+const birth = '1998.04.05'
+console.log(birth)
---
+++ js/j03.js
@@ -0,0 +1,19 @@
+let num1 = 11
+let num2 = 2
+let num3 = -30
+let num4 = 0
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+console.log('------------------------')
+
+let inf = Infinity
+let minf = -Infinity
+let nan = NaN;
+
+console.log(1*'hello')
+console.log(num4 * inf)
