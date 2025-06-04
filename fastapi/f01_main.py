7ee14822bf51ec4be13712f85c9cbc452a498610
n7dd29c <n7dd29c@gmail.com>
2025-06-04 09:00:00 +0900

    ibm x redhat 15일차

---
+++ fastapi/f01_main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/bye/")
+def read_bye():
+    return {"message": "Goodbye!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
